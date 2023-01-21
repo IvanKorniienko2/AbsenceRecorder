16e3a4ccf0ee34f90dedea1469aed30d89566ee5
Ivan Korniienko <Korniienko.I@etoncollege.org.uk>
2023-01-21 22:22:29 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Korniienko, Ivan (PGW) on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
