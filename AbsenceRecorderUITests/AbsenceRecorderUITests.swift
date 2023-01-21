d377e4ff002e1b904c48e5839f41d470cd94ead9
Ivan Korniienko <Korniienko.I@etoncollege.org.uk>
2023-01-21 22:52:40 +0000

    creating groups to structure code

--- AbsenceRecorderUITests/AbsenceRecorderUITests.swift
+++ AbsenceRecorderUITests/AbsenceRecorderUITests.swift
@@ -22,13 +22,8 @@ class AbsenceRecorderUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func example() throws {
+        
     }
 
     func testLaunchPerformance() throws {
