5267e9abdc06e4db3e35b5f68f918cad6f8e7544
Ivan Korniienko <Korniienko.I@etoncollege.org.uk>
2023-02-02 20:27:40 +0000

    part 3 done

---
+++ AbsenceRecorder/StateController.swift
@@ -0,0 +1,16 @@
+//
+//  StateController.swift
+//  AbsenceRecorder
+//
+//  Created by Korniienko, Ivan (PGW) on 02/02/2023.
+//
+
+import Foundation
+
+class StateController: ObservableObject {
+    @Published var divisions: [Division]
+    
+    init() {
+        divisions = Division.examples
+    }
+}
--- AbsenceRecorder/View/DivisionsView.swift
+++ AbsenceRecorder/View/DivisionsView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 
 
-struct ContentView: View {
+struct DivisionsView: View {
     var divisions: [Division]
+    @EnvironmentObject var state: StateController
     @State private var currentDate: Date = Date()
     
     var body: some View {
         NavigationView {
-            List(divisions, id: \.self.code) { division in
+            List(state.divisions, id: \.self.code) { division in
+                NavigationLink(destination: AbsenceView(division: division)) {
                 DivisionItem(division: division)
+                }
             }
             .navigationTitle(currentDate.getShortDate())
             .toolbar {
@@ -37,6 +40,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(divisions: Division.examples)
+        DivisionsView()
+            .environmentObject(StateController())
     }
 }
