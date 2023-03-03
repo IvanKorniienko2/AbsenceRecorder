2d262a0a05df74dd495af2fc1a3dae554d41f600
Ivan Korniienko <Korniienko.I@etoncollege.org.uk>
2023-03-03 10:15:23 +0000

    refactored code that gets the path to a  file in userDocuments

--- AbsenceRecorder/Preview Content/StateController.swift
+++ AbsenceRecorder/Preview Content/StateController.swift
@@ -9,42 +9,20 @@ import Foundation
 import UIKit
 
 class StateController: ObservableObject {
-    @Published var divisions: [Division]
+    @Published var divisions: [Division] = []
     
     init() {
         loadFromFile()
     }
     
     func loadFromFile() {
-        //get url to the file you want to load from
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let userPath = paths[0]
-        let url = userPath.appendingPathComponent("divisions.json")
-        if let data = try? Data(contentsOf: url) {
-            let decoder = JSONDecoder()
-            if let loaded = try? decoder.decode([Division].self, from: data) {
-                divisions = loaded
-            }
+        if let loaded: [Division] = FileManager.default.load(from: "divisions.json") {
+            divisions = loaded
         }
     }
     
     func saveToFile() {
-        //create json encoder
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(divisions) {
-            //take encoded json objects and convert them into String
-            if let json = String(data: encoded, encoding: .utf8) {
-                let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-                let userPath = paths[0]
-                let url = userPath.appendingPathComponent("divisions.json")
-                do {
-                    try json.write(to: url, atomically: true, encoding: .utf8)
-                } catch {
-                    print(error.localizedDescription)
-                    
-                }
-            }
-        }
+        FileManager.default.save(to: "divisions.json", object: divisions)
         
     }
 }
